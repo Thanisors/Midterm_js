6df01a056b6caabfbbb47c9304eb13b1d3072dc8
Thanisors <6206021621031@fitm.kmutnb.ac.th>
2021-02-23 01:55:54 +0700

    up

--- digitalClock.js
+++ digitalClock.js
@@ -1,15 +1,26 @@
 function digitalClock(time){
-    let hours = Math.floor(time / 3600);
-    time -= hours * 3600;
+    hour = Math.floor(time/3600);
+    time-=hour*3600;
+    if(hour < 10){
+        hour = "0"+hour;
+    }
+    if(hour >= 24){
+        hour = "00";
+    }
 
-    let minute = Math.floor(time / 60);
-    time -= minute * 60 ;
+    minute = Math.floor(time/60);
+    time-=minute*60;
+    if(minute < 10){
+        minute = "0"+minute;
+    }
+    if(time < 10){
+        time = "0"+time;
+    }
 
-    
-    alert(time.toLocaleTimeString());
-    time = `${hours} : ${minute} : ${time}`; 
-    return time;
+    mes = hour+":"+minute+":"+time;
+    return mes;
 }
+
 console.log(digitalClock(5025));
 console.log(digitalClock(61201));
 console.log(digitalClock(87000));
